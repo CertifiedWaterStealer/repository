c057ee56884f226ad6ff2b666ba067a2ccac38d8
CertifiedWaterStealer <prasadshivish@gmail.com>
2026-06-11 18:37:09 +1200

    added a script for when quiting, playing the game

---
+++ main-project/script/main_menu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _play() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+	
+func _quit() -> void:
+	get_tree().quit()
+	
+func _option() -> void:
+	print("A WORK IN PROGRESS!!!")

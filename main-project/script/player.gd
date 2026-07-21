3c58f7d96a2809eddf3fec5e7075e4ed7027ac4e
CertifiedWaterStealer <prasadshivish@gmail.com>
2026-07-22 09:55:03 +1200

    Working delay on the stamina bar

--- main-project/script/player.gd
+++ main-project/script/player.gd
@@ -39,22 +39,24 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 
-	if Input.is_action_pressed("shift") and stamina_is_ready: 
+	if Input.is_action_pressed("shift"): 
 		speed = 500.0
 		stamina_ui.value = stamina
 		if velocity.x != 0:
+			stamina_is_ready = false
 			stamina -= 2
 			if stamina < 0:
-				stamina = 0
-		elif stamina < 200:
-			stamina_is_ready = false
-			$Timer.start()
+				stamina = 0 
+	elif stamina_is_ready:
+		if stamina < 200:
 			stamina += 2
+			stamina_ui.value = stamina
 			if stamina > 200:
 				stamina = 200
 				
 	if Input.is_action_just_released("shift"):
 		speed = 300.0
+		$Timer.start()
 		
 	move_and_slide()
 

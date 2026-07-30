96ec2a6677d8758447b2912ec9b6df51cbc88b97
CertifiedWaterStealer <prasadshivish@gmail.com>
2026-07-31 09:37:49 +1200

    made a successful sword animation, that plays when i press 'E'

--- main-project/script/sword.gd
+++ main-project/script/sword.gd
@@ -12,8 +12,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_slash") and is_ready:
+		$Node2D/Sword/AnimationPlayer.play("m1_animation")
 		is_ready = false
-		$Sword/Timer.start()
+		$Node2D/Sword/Timer.start()
 		print("action worked")
 
 

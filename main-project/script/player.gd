6893a6928b1a5962e41322fbf575ef903584c8a6
CertifiedWaterStealer <prasadshivish@gmail.com>
2026-09-23 16:40:46 +1200

    now the game files are fully finished and there are no errors

--- main-project/script/player.gd
+++ main-project/script/player.gd
@@ -4,6 +4,8 @@ var dash_cooldown_timer_is_ready: bool = true
 
 var movement = Vector2()
 
+var health = 10
+
 const SPEED = 200.0
 const SPRINT_SPEED: float = 400.0
 const WALK_SPEED: float = 300.0
@@ -143,7 +145,7 @@ func _on_animated_sprite_2d_animation_finished() -> void:
 	$AnimatedSprite2D/Area2D/sword_collision.disabled = true
 
 func _take_damage():
-	pass
+	health -= 2
 
 # Made to play the animation when the player's 'velocity.x' is not equal to zero, or
 # when the velocity is equal to zero. Fror 'velocity.x' the numbers are oppisite to
--- main-project/script/slime.gd
+++ main-project/script/slime.gd
@@ -19,4 +19,4 @@ func _on_hit_box_area_entered(area: Area2D) -> void:
 
 func _on_hit_box_body_entered(body: Node2D) -> void:
 		if body.has_method("_take_damage"):
-			pass
+			print("it worked")

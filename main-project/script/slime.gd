59cd685709ca67a33c210038cf573e4472214c9a
CertifiedWaterStealer <prasadshivish@gmail.com>
2026-09-26 16:29:21 +1200

    displayable health bar  for enemies

--- main-project/script/slime.gd
+++ main-project/script/slime.gd
@@ -6,9 +6,12 @@ var overlapping: bool = true
 
 var player: CharacterBody2D
 
-@export var animated_sprite: AnimatedSprite2D
+@export var sprite: Sprite2D
+@export var health_ui: ProgressBar
 
 func _ready():
+	health_ui.value = health
+	health_ui.max_value = health
 	for node in get_tree().get_nodes_in_group("Player"):
 		player = node
 
@@ -19,6 +22,9 @@ func _process(_delta: float) -> void:
 func _on_hit_box_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Sword"):
 		health -= 1 
+		health_ui.value = health
+		if not health_ui.visible:
+			health_ui.show()
 		print("sword worked")
 
 func _on_hit_box_body_entered(body: Node2D) -> void:
